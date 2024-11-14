fbd75a175c402f0e2b7f62b37c64e0318a067f19
pkch93 <dev.pkch@gamil.com>
2024-11-14 20:43:15 +0900

    2단계 문자열 계산기 구현

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,21 +1,14 @@
 class Calculator {
     fun calculate(expression: String): Double {
-        val mathExpression = MathExpression(expression)
-        val operands = mathExpression.operands
-        val operators = mathExpression.operators
-        return operators.foldIndexed(operands[0].value) { index, acc, operator ->
-            operator.operate(acc, operands[index + 1].value)
+        val (operands, operators) = parseExpression(expression)
+        var result = operands.first().value
+        operators.forEachIndexed { index, operator ->
+            result = operator.operate(result, operands[index + 1].value)
         }
+        return result
     }
-}
 
-private class MathExpression(
-    expression: String,
-) {
-    val operands: List<Operand>
-    val operators: List<Operator>
-
-    init {
+    private fun parseExpression(expression: String): MathExpression {
         val splitExpression = expression.split(" ")
         val operands = splitExpression
             .filterIndexed { index, _ -> index % 2 == 0 }
@@ -24,21 +17,27 @@ private class MathExpression(
             .filterIndexed { index, _ -> index % 2 == 1 }
             .map { Operator.parse(it) }
 
-        require(operands.size - operators.size == 1) { "피연산자와 연산자의 개수가 맞지 않습니다." }
-
-        this.operands = operands
-        this.operators = operators
+        return MathExpression(operands, operators)
     }
 }
 
-private class Operand(
+data class MathExpression(
+    val operands: List<Operand>,
+    val operators: List<Operator>,
+) {
+    init {
+        require(operands.size - operators.size == 1) { "피연산자와 연산자의 개수가 맞지 않습니다." }
+    }
+}
+
+class Operand(
     value: String?,
 ) {
     val value: Double = value?.toDouble() ?: throw IllegalArgumentException("사칙 연산이 불가능한 값입니다.")
 }
 
-private enum class Operator(
-    private val symbol: String,
+enum class Operator(
+    private val value: String,
 ) {
     PLUS("+") {
         override fun operate(left: Double, right: Double) = left + right
@@ -55,8 +54,8 @@ private enum class Operator(
     ;
 
     companion object {
-        fun parse(symbol: String): Operator {
-            return entries.find { it.symbol == symbol } ?: throw IllegalArgumentException("지원하는 연산자가 아닙니다.")
+        fun parse(value: String): Operator {
+            return entries.find { it.value == value } ?: throw IllegalArgumentException("지원하는 연산자가 아닙니다.")
         }
     }
 
