890e4735f7856173f96deb15cb0a98d4238c973b
pkch93 <dev.pkch@gamil.com>
2024-11-23 10:31:52 +0900

    [step4] 코드리뷰 반영
    - Car#name 필드를 생성자로 정의

--- src/main/kotlin/car/Car.kt
+++ src/main/kotlin/car/Car.kt
@@ -2,13 +2,10 @@ package car
 
 class Car(
     private var position: Int = 0,
-    name: String,
+    private val name: String,
 ) {
-    private val name: String
-
     init {
         require(name.length <= 5) { "자동차 이름은 5자를 초과할 수 없습니다." }
-        this.name = name
     }
 
     fun move(movePossibility: MovePossibility): MovedCar {
